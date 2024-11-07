8a651e2f3b8f12ed0bfce424642c48d2c17a9656
San Linn <sanlin@sans-mbp.lan>
2024-11-07 19:14:25 +0800

    change to fastapi

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import time
-
 import redis
-from flask import Flask
+import uvicorn
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+# cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='127.0.0.1', port=6379)
 
 def get_hit_count():
     retries = 5
@@ -17,7 +21,20 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-@app.route('/')
-def hello():
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    # count = 0
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    print(count)
+    return templates.TemplateResponse(
+        request=request, name="home.html", context={"count": count}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), log_level="info")
